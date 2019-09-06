b71ad1a272ec6e7a83b4fefcf88a121f6b50a9c2
Tyler Schaffer <tpschaffer@gmail.com>
2019-09-06 10:10:22 -0600

    Clean up validations and validation tests

--- app/models/snack.rb
+++ app/models/snack.rb
@@ -1,5 +1,6 @@
 class Snack < ApplicationRecord
-  validates_presence_of :name, :price
+  validates_presence_of :name
+  validates_numericality_of :price
 
   has_many :machine_snacks
   has_many :machines, through: :machine_snacks
--- spec/models/machine_spec.rb
+++ spec/models/machine_spec.rb
@@ -3,6 +3,9 @@ require 'rails_helper'
 RSpec.describe Machine, type: :model do
   describe 'validations' do
     it { should validate_presence_of :location }
+  end
+
+  describe 'relationships' do
     it { should belong_to :owner }
     it { should have_many :machine_snacks }
     it { should have_many(:snacks).through(:machine_snacks) }
@@ -19,7 +22,7 @@ RSpec.describe Machine, type: :model do
       @sam_machine_1.snacks << @snack_2
       @sam_machine_1.snacks << @snack_3
     end
-
+ 
     it "can calculate average price" do
       expect(@sam_machine_1.average_price).to eq(2.5)
     end
